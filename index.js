bba43650c653b5fdecb4b59e762dfeb8c04fbf04
karnpapon <karnpapon@gmail.com>
2019-04-21 23:58:41 +0700

    *

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 
 // visual part ( clear distractive DOM element).
 let header = document.getElementsByClassName("container")
-let navbar = document.getElementsByClassName("navbar")
-header[0].style.opacity = 0
-navbar[0].style.opacity = 0
-document.body.style.paddingTop = 0
+let slide = document.getElementsByClassName("ui-slider-handle")
+let listSocial = document.getElementById("navbar").querySelectorAll("div>a");
+for (list of listSocial) { list.remove() }
+for (item of slide) { item.remove() }
+// header[0].style.opacity = 0
+// navbar[0].style.opacity = 0
+// document.body.style.paddingTop = 0
 
 // get elements.
 let btn = document.getElementsByClassName("con-playpause")
 let start = document.getElementById('begin')
 let end = document.getElementById("end")
 
-// controls.
-btn[0].click()
+
 
 const getRandomInt = function(min, max) {
     min = Math.ceil(min);
@@ -21,46 +23,55 @@ const getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var startPos = 16
+var startFactor = 16
 var rand
 var randClock = getRandomInt(20, 200)
 var randClockNarrow = getRandomInt(20, 50)
 var baseClock = 200
 var baseClockNarrow = 100
-var timer, timerPos
+var endPos, startPos
 
-timer = setInterval( function(){
-  rand = Math.random() + startPos
+endPos = setInterval( function(){
+  rand = Math.random() + startFactor
   end.value = rand
 }, baseClockNarrow + randClock)
 
-timerPos = setInterval( function(){
-    startPos = getRandomInt(39, 50)
-    start.value = startPos
+startPos = setInterval( function(){
+    startFactor = getRandomInt(0, 20)
+    start.value = startFactor
 }, baseClock + randClock)
 
 
 // relative end position.
-end.value = parseFloat(start.value) + 0.02
+// end.value = parseFloat(start.value) + 0.02
 
 const hold = function(){
-    clearTimeout(timer)
-    clearTimeout(timerPos)
+    clearTimeout(endPos)
+    clearTimeout(startPos)
 }
 
 const resume = function(){
-    timer = setInterval( function(){
-        rand = Math.random() + startPos
+    endPos = setInterval( function(){
+        rand = Math.random() + startFactor
         end.value = rand
-      }, baseClockNarrow + randClock)
+    }, baseClockNarrow + randClock)
       
-      timerPos = setInterval( function(){
-          startPos = getRandomInt(39, 50)
-          start.value = startPos
-      }, baseClock + randClock)
+    timerStart = setInterval( function(){
+          startFactor = getRandomInt(startFactor ,50 )
+          start.value = startFactor
+    }, baseClock + randClock)
+
+    startPos = setInterval(function () {
+        startFactor = getRandomInt(0, 20)
+        start.value = startFactor
+    }, baseClock + randClock)
 }
 
 const stop = function(){
     hold()
     btn[0].click()
 }
+
+
+// controls.
+btn[0].click()
